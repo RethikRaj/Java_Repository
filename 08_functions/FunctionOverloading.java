9c4ca60932cae10cb5c12e8fbd454bda2e0d8c33
RethikRaj <rethikrajrr@gmail.com>
2026-03-02 18:59:27 +0530

    Add Basics and FunctionOverloading classes with examples of functions and overloading

---
+++ 08_functions/FunctionOverloading.java
@@ -0,0 +1,26 @@
+public class FunctionOverloading {
+    public static void main(String[] args) {
+        
+    }
+
+    // 1. Based on number of parameter
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // 2. Based on data type of parameter
+    static int sum(int a, double b) {
+        return a + (int)b;
+    }
+
+    // 3. Based on order of parameter
+    static int sum(double a, int b) {
+        return (int)a + b;
+    }
+
+    // Note : functions cannot be overloaded based solely on their return type
+}
